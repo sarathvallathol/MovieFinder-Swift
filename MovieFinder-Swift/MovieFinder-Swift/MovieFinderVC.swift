146a26ebad77d408d7e287675f78d6eeedc371d1
sarath <sarath@askme.in>
2018-05-17 19:48:17 +0530

    viewcontroller with table view and search bar created

---
+++ MovieFinder-Swift/MovieFinder-Swift/MovieFinderVC.swift
@@ -0,0 +1,59 @@
+//
+//  MovieFinderVC.swift
+//  MovieFinder-Swift
+//
+//  Created by sarath on 17/05/18.
+//  Copyright © 2018 com.sarath.homeProduction. All rights reserved.
+//
+
+import UIKit
+
+class MovieFinderVC: UIViewController {
+
+    @IBOutlet weak var movieSearchBar: UISearchBar?
+    @IBOutlet weak var resultTeableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension MovieFinderVC:UITableViewDelegate{
+    
+}
+extension MovieFinderVC:UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        
+    }
+    
+    
+}
---
+++ MovieFinder-Swift/MovieFinder-Swift/ResultViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResultViewCell.swift
+//  MovieFinder-Swift
+//
+//  Created by sarath on 17/05/18.
+//  Copyright © 2018 com.sarath.homeProduction. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
