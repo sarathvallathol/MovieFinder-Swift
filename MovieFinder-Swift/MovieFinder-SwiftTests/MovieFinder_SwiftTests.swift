304e2ab963fb10a82bd08e6752af2e7374a3dc8b
sarath <sarath@askme.in>
2018-05-17 19:10:16 +0530

    Created a project

---
+++ MovieFinder-Swift/MovieFinder-SwiftTests/MovieFinder_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  MovieFinder_SwiftTests.swift
+//  MovieFinder-SwiftTests
+//
+//  Created by sarath on 17/05/18.
+//  Copyright © 2018 com.sarath.homeProduction. All rights reserved.
+//
+
+import XCTest
+@testable import MovieFinder_Swift
+
+class MovieFinder_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
